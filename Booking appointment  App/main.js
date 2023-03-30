c3a407185df4fe662100a6ebb827dcc4d88279dd
Anuppaswan24 <119486812+Anuppaswan24@users.noreply.github.com>
2023-03-30 04:58:53 -0700

    Add files via upload

---
+++ Booking appointment  App/main.js
@@ -0,0 +1,57 @@
+// single element
+console.log(document.getElementById('mu-form'));
+console.log(document.querySelector("h1"));
+
+// multiple Element
+// console.log(document.querySelectorAll('.item'));
+// console.log(document.getElementsByClassName('item'));
+//  console.log(document.getElementsByTagName('li'));
+
+// const items = document.querySelectorAll('.item');
+// items.forEach((item) => console.log(item));
+
+const ul = document.querySelector('.items');
+
+// // ul.remove();
+// ul.lastElementChild.remove();
+// ul.firstElementChild.textContent='anup';
+// ul.children[1].innerText = 'brad';
+// ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+// const btn =document.querySelector('.btn');
+// btn.style.background = 'red';
+// btn.addEventListener('click',(e) => {
+//     e.prenetDefault();
+//     document.querySelector('#my-form').style.background='#ccc';
+//     document.querySelector('body').classList.add('bg-dark');
+//     document.querySelector('.items')
+//     .lastElementChild.innerHtml = '<h1>Hello</h1>';
+// });
+
+const myForm = document.querySelector('#my-form');
+const nameInput= document.querySelector('#name');
+const emailInput= document.querySelector('#email');
+const msg = document.querySelector('.msg')
+const userList= document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+
+    if(nameInput.value === '' || emailInput.value === ''){
+       msg.classList.add('error');
+        msg.innerHTML = 'please enter all fields';
+        setTimeout(() => msg.remove(), 3000);
+    }else{
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+            userList.appendChild(li);
+
+            nameInput.value='';
+            emailInput.value = '';
+    } 
+}    
+
+
